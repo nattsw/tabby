dd25b4b2181c794de130d5da31bdeab4c9199a0c
Natalie Tay <natalie.tay@gmail.com>
2016-03-03 16:47:24 +0800

    Initial commit...
    ...except with Twitter API set up using fabric already.

---
+++ app/src/main/java/sg/lie/nata/ankomedia/activities/MainActivity.java
@@ -0,0 +1,15 @@
+package sg.lie.nata.ankomedia.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import sg.lie.nata.ankomedia.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
